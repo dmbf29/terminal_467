a6f8cee12bcdc7016e234b0803a68cb603446b4f
Douglas Berkley <douglasmberkley@gmail.com>
2020-09-26 15:18:22 +0900

    created terminal cheetsheet and basic html file.

---
+++ cheatsheet.rb
@@ -0,0 +1,32 @@
+pwd
+-> prints current directory
+
+ls / ll
+-> lists the contents of directory
+
+cd directory
+-> moves into a directory
+
+cd ..
+  -> moves backwards
+
+mkdir
+-> creates new directory
+
+touch
+-> creates new file
+
+rm filename
+-> removes file
+
+rm -r directory
+-> removes directory
+
+rm -f something
+-> force removes
+
+mv file_name directory
+-> moves into directory
+
+mv file_name new_file_name
+-> renames
